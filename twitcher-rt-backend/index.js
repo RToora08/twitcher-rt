c6408e435e13dbc664db6cc600bd646b23a7cf9a
Rahul2Ra <rahultoora1008@gmail.com>
2020-04-14 14:25:33 -0400

    errorHandler added

--- twitcher-rt-backend/index.js
+++ twitcher-rt-backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const errorHandler = require('./handlers/error.js');
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-	res.send('Hello World!');
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, function() {
 	console.log(`Serving warbler app on port ${PORT}`);
