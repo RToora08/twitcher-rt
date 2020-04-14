88fa84ba82d2645be2a15901555320dae8c1fd6d
Rahul2Ra <rahultoora1008@gmail.com>
2020-04-14 14:31:43 -0400

    old version deleted

--- twitcher-rt_backend/handlers/error.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-	return res.status(err.status || 500).json({
-		error: {
-			message: err.message || 'Something Went Wrong'
-		}
-	});
-};
-
-module.exports = errorHandler;
